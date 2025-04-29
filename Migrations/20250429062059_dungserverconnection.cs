bad879fedc9e0d236462897f28969ad7859c11ef
Nguyen Minh Tam <tamn8477@gmail.com>
2025-04-29 13:22:28 +0700

    quen update-database

---
+++ Migrations/20250429062059_dungserverconnection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Websitecanhan.Migrations
+{
+    /// <inheritdoc />
+    public partial class dungserverconnection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
